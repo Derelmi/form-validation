d38e274450f645dd1b4021683725470a2c6fb653
Derelmi <richardnjom12@gmail.com>
2022-04-16 02:12:04 -0700

    new updates

---
+++ Validate_form.js
@@ -0,0 +1,8 @@
+ function validate(){
+            if(document.myform.name=""){
+                alert("Please provide your name!");
+                // document.myform.name.focus();
+                return false;
+            }
+            // return true;
+        }
