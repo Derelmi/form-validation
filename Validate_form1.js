fcbf508e9a38ad4f958b192fa6b22dc54c4b6a13
Derelmi <richardnjom12@gmail.com>
2022-04-18 13:49:55 -0700

    loading

--- Validate_form1.js
+++ Validate_form1.js
@@ -3,10 +3,29 @@ const form = document.getElementById('register-form')
  const firstname = document.getElementById('firstName')
  const lastname = document.getElementById('lastName')
  const address = document.getElementById('address')
- const error = document.getElementById('error')
+ const errorElement = document.getElementById('error')
 
  form.addEventListener('submit', e => {
-     e.preventDefault();
+     let messages = {}
+      if(firstnameValue.trim() =="" || lastnameValue.trim() =="" ){
+          masseges.push('Name is required')
+      }
+      if(firstname.lenghth >=50 || lastname.lenghth >=50 ){
+          masseges.push('Your name should not be longer than 50')
+      }
+
+      if(addressValue.trim() =="" ){
+            masseges.push('address is required')
+        }
+        if(address.lenghth >=300 ){
+          masseges.push('Your address should not be longer than 300 characters')
+      }
+
+      
+    //   if (messages.lenghth >0 {
+     e.preventDefault()
+    //    errorElement.innText = messages.join(',')
+//  }
 
      validateInputs();
  });
