c6568a048713269f40aec52ef80b62957e90bd5b
Derelmi <richardnjom12@gmail.com>
2022-04-18 02:11:11 -0700

    updates

--- Validate_form.js
+++ Validate_form.js
@@ -1,8 +1,40 @@
+ const form = document.getElementById('register-form')
+ const firstname = document.getElementById('fname')
+ const lastname = document.getElementById('lname')
+ const address = document.getElementById('address')
+
+ form.addEventListener('submit', e => {
+     e.preventDefault();
+
+     validateInputs();
+ });
+
+ const setError = (element,message) => {
+     const inputcontrol = element.parentElement;
+     const errorDisplay = inputcontrol.querySelection('.error');
+
+    //  errorDisplay.innerText = message;
+    //  inputcontrol.classlist.add('error');
+    //  inputcontrol.classlist.remove('success');
+
+ }
+
+ const validateInputs = () => {
+     const firstnameValue = fname.value.trim();
+     const lastnameValue = fname.value.trim();
+     const addressValue = address.value.trim();
+
+     if(firstnameValue ===''){
+
+     }
+ }
+ 
  function validate(){
-            if(document.myform.name=""){
+            if(document.myform.id="register-form"){
                 alert("Please provide your name!");
                 // document.myform.name.focus();
                 return false;
             }
-            // return true;
+            // return true; 
         }
+
